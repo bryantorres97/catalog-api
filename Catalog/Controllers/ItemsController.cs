28c4ca4bdb45241f521e1e9bb36f83df6623aee9
Bryan Torres <bryantorresckp@gmail.com>
2021-11-17 16:19:11 -0500

    Clase de Item agregada

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+
+namespace Catalog.Controllers
+{
+
+    // GET /items
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+    }
+}
---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.Entities
+{
+    public class Item
+
+    {
+        public Guid Id { get;init; }
+        public string Name { get;init;}
+
+        public decimal Price { get;init;}
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+    }
+}
