be85ad6415b5ed876db154325eb8c2cc1bff6067
Bryan Torres <bryan_torres@outlook.com>
2021-11-17 21:11:12 -0500

    Endpoint GetItems creado

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,15 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.Entities;
 using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.Controllers
 {
-
-    // GET /items
     [ApiController]
-    [Route("items")]
+    [Route("[controller]")]
     public class ItemsController : ControllerBase
     {
         private readonly InMemItemsRepository repository;
 
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Item> GetItems()
+        {
+            IEnumerable<Item> items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public Item GetItem(Guid id)
+        {
+            Item item = repository.GetItem(id);
+            return item;
+        }
+        
     }
 }
